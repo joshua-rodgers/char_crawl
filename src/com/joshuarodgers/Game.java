fcd2a6fa24b2523597cdd78ad716f0b3e62dbc76
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2021-03-05 11:35:52 -0600

    doors placed, test version
    doors placed in a chain. west wall to east wall of next room. spacebar wired up to iterate through rooms. Game.test_next_room() might become permanent at some point, not sure. depends on what i do in use_door(). this was challenging. i'm loving this, but its a warning to keep my additions simple. it wouldve been unmanageable to do doors in random locations on any of the four walls at one go.

--- src/com/joshuarodgers/Game.java
+++ src/com/joshuarodgers/Game.java
@@ -8,6 +8,7 @@ public class Game{
     char[][] current_board;
     Gamepiece[][][] dungeon;
     Gamepiece [][] current_map;
+    int current_map_idx;
     Player player;
     Gamepiece[] pieces;
 
@@ -30,12 +31,16 @@ public class Game{
         for(var room = 0; room < dungeon.length; room++){
             dungeon[room] = new Gamepiece[g_utilities.get_random(4, limit)][g_utilities.get_random(4, limit)];
         }
+
         Gamepiece[][] first_room = dungeon[0];
+        current_map_idx = 0;
         current_board = new char[first_room.length][first_room[0].length];
 
         for(Gamepiece[][] room:dungeon){
             build_room(room);
         }
+
+        place_doors(dungeon);
 
         current_map = first_room;
         player = new Player(current_map);
@@ -47,7 +52,7 @@ public class Game{
     }
 
     private void build_room(Gamepiece[][] room){
-        int num_doors = g_utilities.get_random(4);
+        
         for(int row = 0; row < room.length; row++){
             for(int col = 0; col < room[row].length; col++){
                 if(row == 0 || row == room.length - 1){
@@ -62,8 +67,65 @@ public class Game{
         }
     }
 
-    private void place_doors(Gamepiece[][] room){
+    private void place_doors(Gamepiece[][][] dungeon){
+        //int start_idx = g_utilities.get_random(dungeon_size);
+        int rem_doors = dungeon_size - 1;
+        int last_idx = 0;
 
+        Gamepiece[][] current_room = dungeon[0];
+        int current_idx = 0;
+
+        int last_wall = -1;
+
+        while(rem_doors > 0){
+            //int wall = g_utilities.get_random(4);
+            int wall = 0;
+            int position;
+            Door current_door = new Door(false);
+            switch(wall){
+                case 0:
+                    position = g_utilities.get_random(1, current_room.length - 2);
+                    if(current_idx < dungeon.length - 1){
+                        if(dungeon[current_idx][position][0].glyph == 'X'){
+                            dungeon[current_idx][position][0] = current_door;
+                        }else{
+                            break;
+                        }
+                        
+                        current_door.set_first(position, 0, current_room);
+                        current_idx++;
+                        current_room = dungeon[current_idx];
+                        position = g_utilities.get_random(1, current_room.length - 2);
+                        dungeon[current_idx][position][dungeon[current_idx][position].length - 1] = current_door;
+                        current_door.set_second(position, dungeon[current_idx][position].length - 1, current_room);
+                        rem_doors--;
+                    }
+                    
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+
+    public void test_next_room(){
+        if(current_map_idx < dungeon.length - 1){
+            current_map_idx++;
+            current_map = dungeon[current_map_idx];
+            current_board = new char[current_map.length][current_map[0].length];
+        }else{
+            current_map_idx = 0;
+            current_map = dungeon[current_map_idx];
+            current_board = new char[current_map.length][current_map[0].length];
+        }
     }
 
     public void update(){
