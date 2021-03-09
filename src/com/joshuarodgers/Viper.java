d2501b35c13ca29e22342e8de0b6ba5971a36e74
Joshua Rodgers <34038374+joshua-rodgers@users.noreply.github.com>
2021-03-08 21:16:36 -0600

    threads?
    perhaps each enemy lives in it own thread? makes sense. each one needs to move and 'think' jsut like the player

--- src/com/joshuarodgers/Viper.java
+++ src/com/joshuarodgers/Viper.java
@@ -1,15 +1,16 @@
 package com.joshuarodgers;
 
-public class Viper extends Enemy {
+public class Viper extends Enemy implements Runnable {
     public Viper(int row, int col, Gamepiece[][] room){
         this.name = "Viper";
         this.glyph = 'v';
         this.row = row;
         this.col = col;
+        this.is_alive = true;
         this.room = room;
     }
 
-    public void live(){
-        
+    public void run(){
+
     }
 }
